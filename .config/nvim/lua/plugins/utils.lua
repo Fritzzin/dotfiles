4cb2c08c141f22fb0e680ec5b19526bde5aaf2cd
Augusto Fritz <augustofritzz@gmail.com>
2022-09-26 10:45:34 -0300

    Adding neovim config

---
+++ .config/nvim/lua/plugins/utils.lua
@@ -0,0 +1,115 @@
+require'nvim-web-devicons'.get_icons()
+-- Para Barra de status
+require("lfs")
+
+local function dot_git_exists()
+  local path = "./.git"
+  if (lfs.attributes(path, "mode") == "directory") then
+    return true
+  end
+  return false
+end
+
+if dot_git_exists() then
+  branch = '-branch'
+else 
+  branch = '-📁'
+  --branch = '-  '
+end
+
+local function get_var(my_var_name)
+  return vim.api.nvim_get_var(my_var_name)
+end
+
+extension = get_var("extension")
+
+if extension == "cpp" or extension == "hpp" or extension == "h" then
+  this_lsp = '-lsp_name'
+else
+  this_lsp = '-file_size'
+end
+
+require('staline').setup {
+    defaults = {
+        expand_null_ls = false,  -- This expands out all the null-ls sources to be shown
+        left_separator  = "",
+        right_separator = "",
+        full_path       = false,
+        line_column     = "[%l/%L] :%c 並%p%% ", -- `:h stl` to see all flags.
+
+        fg              = "#000000",  -- Foreground text color.
+        bg              = "none",     -- Default background is transparent.
+        inactive_color  = "#303030",
+        inactive_bgcolor = "none",
+        true_colors     = false,      -- true lsp colors.
+        font_active     = "none",     -- "bold", "italic", "bold,italic", etc
+
+        mod_symbol      = "  ",
+        lsp_client_symbol = " ",
+        branch_symbol   = " ",
+        cool_symbol     = "",       -- Change this to override default OS icon.
+        null_ls_symbol = "",          -- A symbol to indicate that a source is coming from null-ls
+    },
+    mode_colors = {
+        n = "#2bbb4f",
+        i = "#986fec",
+        c = "#e27d60",
+        v = "#4799eb",   -- etc..
+    },
+    mode_icons = {
+        n = " ",
+        i = " ",
+        c = " ",
+        v = " ",   -- etc..
+    },
+    sections = {
+        left = { '- ', '-mode', 'left_sep_double', ' ', 'branch' },
+        mid  = { 'file_name' },
+        right = { 'cool_symbol','right_sep_double', '-line_column' },
+    },
+    special_table = {
+        NvimTree = { 'NvimTree', ' ' },
+        packer = { 'Packer',' ' },        -- etc
+    },
+    lsp_symbols = {
+        Error=" ",
+        Info=" ",
+        Warn=" ",
+        Hint="",
+    },
+}
+
+-- Whitespace / Colors indent lines
+vim.opt.listchars:append("space:⋅")
+--vim.opt.listchars:append("eol:↴")
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#FF5555 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#F1FA8C gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#50FA7B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#8BE9FD gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#BD93F9 gui=nocombine]]
+
+vim.cmd([[
+  hi! MatchParen cterm=NONE,bold gui=NONE,bold guibg=NONE guifg=#FFFFFF
+  let g:indentLine_fileTypeExclude = ['dashboard']
+]])
+
+require("indent_blankline").setup {
+    show_end_of_line = true,
+    space_char_blankline = " ",
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+    },
+}
+
+-- COLORIZER
+require'colorizer'.setup()
+
+require('nvim-autopairs').setup({
+  enable_check_bracket_line = false
+})
