a789bb7ceb8f2191c3123ecb8ef6249a7593d01c
Augusto Fritz <fritzzin@Augustos-MacBook-Pro.local>
2024-02-28 15:56:26 -0300

    First backup of neovim configuration

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,20 @@
+vim.cmd("set termguicolors")
+vim.cmd("set t_Co=256")
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number relativenumber")
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>ob", '<cmd>!open "%"<cr>', { desc = "Open file in Chrome" })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
